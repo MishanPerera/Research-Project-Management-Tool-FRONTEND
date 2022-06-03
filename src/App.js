6c6036385bb41496a4ee37ed9a04ed776f46a9ce
InsafNilam <89188261+InsafNilam@users.noreply.github.com>
2022-06-03 20:25:07 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
+import React from "react";
 import './App.css';
+import {BrowserRouter as Router , Switch, Route} from 'react-router-dom';
+import {Paper, Typography} from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import LoginForm from "./Components/Admin/LoginForm";
+import RoleSelector from "./RoleSelector";
+//Implement Protected Route For the Components
+import ProtectedRoute from './ProtectedRoute';
+import AdminHome from "./Components/Admin/Home";
+import User from './Components/Admin/User'
+import Document from "./Components/Admin/Document";
+import Group from "./Components/Admin/Group";
+
+
+function App(){
+    const isAuth = sessionStorage.getItem("isAuth");
+    return(
+    <>
+    <Router>
+        <Switch>
+            <Route exact path='/' component={!isAuth ? RoleSelector : AdminHome}/>
+            <Route exact path='/login/:role' component={!isAuth ? LoginForm : AdminHome}/>
+            <ProtectedRoute path='/home' exact component={AdminHome} auth={isAuth}/>
+            <ProtectedRoute path='/user' exact component={User} auth={isAuth}/>
+            <ProtectedRoute path='/document' exact component={Document} auth={isAuth}/>
+            <ProtectedRoute path='/group' exact component={Group} auth={isAuth}/>
+        </Switch>
+    </Router>
+    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+        <Typography variant="body2" color="text.secondary" bgcolor='gray' align='center'>
+            Copyright &copy; 2022, DEVX Pvt. Ltd.
+        </Typography>
+    </Paper>
+    </>
+    )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App'
+import Document from './Components/Admin/Document'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-
-reportWebVitals();
+ReactDOM.render(<App/>, document.querySelector("#root"));
